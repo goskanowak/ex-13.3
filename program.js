f473b6c87eb7b7386e65aa5073375cbf8a53f76c
Małgorzata Nowak <goskanowak87@gmail.com>
2018-05-10 13:03:08 +0200

    Add program.js

--- program.js
+++ program.js
@@ -4,12 +4,28 @@ process.stdin.on('readable', function() {
     var input = process.stdin.read();
     if(input !== null) {
         var instruction = input.toString().trim();
-        if (instruction === '/exit') {
-            process.stdout.write('Quitting app!\n');
-            process.exit();
-        } else {
-            process.stderr.write('Wrong instruction!');
+//        if (instruction === '/exit') {
+//            process.stdout.write('Quitting app!\n');
+//            process.exit();
+//        } else {
+//            process.stderr.write('Wrong instruction!');
+//        }
+        switch (instruction) {
+            case '\info':
+                console.log(process.env);
+                break;
+            case '\hi':
+                process.stdout.write('Hello\n');
+                break;
+            case '\exit':
+                process.stdout.write('Quitting app!\n');
+                process.exit();
+                break;
+            default:
+                 process.stderr.write('Wrong instruction!\n');
+                break;
         }
     }
-}); // nasłuchujemy zdarzenie odczytu i wtedy wykonuje funkcje, zmienna z metodą .read() ma za zadanie odczytać co użytkownik podał na wejściu, medtoda if jeżeli wartość jest inna niż null, ma wyświetlić. warunek sprawdza czy komenda podana przez użytkownika jest poprawna. metoda trim - usówa białe znaki.
+}); 
 
+// nasłuchujemy zdarzenie odczytu i wtedy wykonuje funkcje, zmienna z metodą .read() ma za zadanie odczytać co użytkownik podał na wejściu, medtoda if jeżeli wartość jest inna niż null, ma wyświetlić. warunek sprawdza czy komenda podana przez użytkownika jest poprawna. metoda trim - usuwa białe znaki.
